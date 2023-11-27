232782c7ad2a074ec096555fda115712c6e00368
azro <35503478+azro352@users.noreply.github.com>
2023-11-27 23:06:50 +0100

    add logs

--- build.sh
+++ build.sh
@@ -6,20 +6,30 @@ if ! [ -x "$(command -v git)" ]; then
   exit 1
 fi
 
+echo "$(date "+%H:%M:%S"): START CLONE"
 git clone https://github.com/ant-design/ant-design.git
+echo "$(date "+%H:%M:%S"): END CLONE"
 
+echo "$(date "+%H:%M:%S"): START DL IMAGES"
 python ant_design_dl_img.py ant-design/components
 chmod +x seds.sh
+echo "$(date "+%H:%M:%S"): END DL IMAGES"
 
 cd ant-design
 git checkout gh-pages
 
+echo "$(date "+%H:%M:%S"): START DL IMAGES"
 grep -HEorna "https.*huamei.*img/.{32}" components/ | cut -f1 -d: >> files_to_sed
 grep -HEorna "https.*cRK" ./ | cut -f1 -d: >> files_to_sed
+echo "$(date "+%H:%M:%S"): END DL IMAGES"
 
 echo "Len files_to_sed $(wc -l files_to_sed)"
 
+echo "$(date "+%H:%M:%S"): START SED PATHS"
 cat files_to_sed | xargs -iZ ../seds.sh Z
+echo "$(date "+%H:%M:%S"): END SED PATHS"
+
+rm -rf .git
 
 cd ..
 
